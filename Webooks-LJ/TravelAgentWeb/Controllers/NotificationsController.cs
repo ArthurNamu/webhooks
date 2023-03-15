1059419c1817fb520ac3dabad964051d4ba55a2c
Arthur Namu <ArthurN@hollywoodbets.net>
2023-03-15 09:53:18 +0300

    Added webhook endpoint to Travel Agent

---
+++ Webooks-LJ/TravelAgentWeb/Controllers/NotificationsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelAgentWeb.Data;
+using TravelAgentWeb.Dtos;
+
+namespace TravelAgentWeb.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class NotificationsController : ControllerBase
+{
+    private readonly TravelAgentDbContext _context;
+
+    public NotificationsController(TravelAgentDbContext context)
+	{
+        _context = context;
+
+    }
+
+    [HttpPost]
+    public ActionResult FlightChanges(FlightDetailUpdateDto flightDetailUpdateDto)
+    {
+        Console.WriteLine($"--> Webhook received from : {flightDetailUpdateDto.Publisher}");
+
+        var secretModel = _context.SubscriptionSecrets.FirstOrDefault(s =>
+        s.Publisher == flightDetailUpdateDto.Publisher && s.Secret == flightDetailUpdateDto.Secret);
+
+        if(secretModel == null)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid Secret!! - Ignore webhook!!");
+            Console.ResetColor();
+            return Ok();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Valid Webhook - Proceed to process!!");
+            Console.WriteLine($"Old price {flightDetailUpdateDto.OldPrice},  New Price {flightDetailUpdateDto.NewPrice} ");
+            Console.ResetColor();
+            return Ok();
+        }
+    }
+}
--- Webooks-LJ/TravelAgentWeb/Program.cs
+++ Webooks-LJ/TravelAgentWeb/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TravelAgentWeb.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var Configuration = builder.Configuration;
+
 // Add services to the container.
+builder.Services.AddDbContext<TravelAgentDbContext>(opt =>
+opt.UseSqlServer(Configuration.GetConnectionString("TravelAgentConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
